a57e0ea5ca032fdb17e1854d2893560df33de8fc
Caio Gomes <caiogomesteodoro21@gmail.com>
2023-07-27 23:45:45 -0300

    add produtos

--- Produtos.js
+++ Produtos.js
@@ -3,28 +3,26 @@ let produtoJson = [
     id: 1,
     name: "Guarda-Chuva",
     img: "/img/p001.png",
-    price: 69.8,
+    price: 18.9,
     sizes: ["100g", "530g", "860g"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
   },
   {
     id: 2,
-    name: "Calabresa",
-    img: "images/pizza2.png",
-    price: 18.0,
+    name: "Lampião De Led Luminária",
+    img: "/img/p002.png",
+    price: 69.8,
     sizes: ["320g", "530g", "860g"],
-    description:
-      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+    description: "Recarregável 8w 36 Leds | Bivolt",
   },
   {
     id: 3,
-    name: "Quatro Queijos",
-    img: "images/pizza3.png",
-    price: 17.45,
+    name: "Raquete Elétrica Mata Mosquito Dengue Insetos ",
+    img: "/img/p003.png",
+    price: 25.9,
     sizes: ["320g", "530g", "860g"],
-    description:
-      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+    description: "Recarregável | Bivolt",
   },
   {
     id: 4,
